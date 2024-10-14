140fccee04ff0e3a399f8a9225bfa616140ba71f
Bayram OZKAN <73391169+nakzoo@users.noreply.github.com>
2024-10-14 18:09:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express()
 app.get('/', (req, res) => {
     res.json({
         status: 200,
-        message: "hello my f*ckinglife"
+        message: "---- bb world  ----"
+        
     })
 })
 
